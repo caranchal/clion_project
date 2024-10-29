2ddd0e7b9611c1ca24b4e53503f65ea89d2ab83d
abrikosik <abrikospd2@mail.ru>
2024-10-29 15:21:39 +0700

    kuku

---
+++ triangle.cpp
@@ -0,0 +1,12 @@
+//
+// Created by abrikosik on 28.10.2024.
+//
+#include <iostream>
+int triangle(int x, int y, int z){
+    int perimetr = x + y + z;
+
+}
+int main(){
+    std::cout<<triangle(2,2,3);
+
+}
