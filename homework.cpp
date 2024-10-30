70e309ce279fc397a060cecee3628ac1d24b53b6
abrikosik <abrikospd2@mail.ru>
2024-10-30 13:22:28 +0700

    кайф

--- homework.cpp
+++ homework.cpp
@@ -6,7 +6,7 @@
 #include "math.h"
 using namespace std;
 int resistance(double R1, double R2, double R3){ //округляет результат -
-    double answer = (1/(1 / R1) + 1 / R2 + (1 / R3));
+    double answer = ((1/(1 / R1) + 1 / R2 + (1 / R3)));
     return answer;
 }
 int cordinate(float x1, float y1, float x2, float y2){ //округляет результат -
@@ -50,10 +50,6 @@ void value_n_lenght(int n){
 
     }
     cout<<"в данном числе "<<i<<" символов"<<endl;
-
-
-
-
 }
 void value_n_sum(int n){
     int sum_value;
@@ -65,13 +61,40 @@ void value_n_sum(int n){
     }
     cout<<sum_value;
 }
-int main(){
+void sum_value(int n){
+    int sum_value = 0;
+    while (n != 0) {
+        sum_value += n%10;
+        n /= 10;
+        sum_value = sum_value;
+    }
+    cout << sum_value<<endl;
+}
+void last_value(int value){
+    int last = value % 10;
+    cout<<last<<endl;
+}
+void first_value(int value){
+    if(value < 10){
+       cout<<value<<endl;
+    }
+    if(value > 10 && value << 100){
+        cout<<value / 10<<endl;
+    }
+    else{
+        cout<< value / 100<<endl;
+    }
+}
+int main() {
     SetConsoleOutputCP(CP_UTF8);
 
-    cout<<resistance(2,2,3)<<endl;
-    cout<<cordinate(1.0,2.0,-3.0,4.0)<<endl;
-    ravennstvo(3,2,3);
-    ravenstvo_3(1,4,1);
+    cout << resistance(1, 2, 3) << endl;
+    cout << cordinate(1.0, 2.0, -3.0, 4.0) << endl;
+    ravennstvo(3, 2, 3);
+    ravenstvo_3(1, 4, 1);
     value_n_lenght(56);
-    value_n_sum(100);
+    sum_value(56);
+    last_value(2);
+    first_value(45);
+    return 0;
 }
