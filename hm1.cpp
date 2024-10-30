2334acf95c17f621e46147e62554611eb6b55d96
abrikosik <abrikospd2@mail.ru>
2024-10-30 13:47:35 +0700

    алгоритм

---
+++ hm1.cpp
@@ -0,0 +1,11 @@
+//
+// Created by abrikosik on 30.10.2024.
+//
+#include <iostream>
+#include "windows.h"
+using namespace std;
+int main(){
+    SetConsoleOutputCP(CP_UTF8);
+    cout<<"поход в магазин\n---------------------\n";
+    cout<<"1. одеться \n----------------------\n2. выйти из дома \n----------------------\n3. дойти до магазина \n----------------------\n4. купить пива\n----------------------\n";
+}
