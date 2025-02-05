ab5b6a35ec5be740c96d0f05fd36ce46f2f20afe
caranchal <109738188+caranchal@users.noreply.github.com>
2025-02-05 13:57:39 +0700

    Update class2.cpp

--- class2.cpp
+++ class2.cpp
@@ -60,13 +60,14 @@ public:
     void pushMoney(int money) {
         this->bankMoney -= money;
         this->plussMoneyA(money);
-        
+
 
     }
-    void pushToClient( int money) {
+    void pushToClient(int money) {
         this->minusMoneyA(money);
         this->plussMoneyB(money);
     }
+
 };
 
 
@@ -102,8 +103,8 @@ public:
     Currency sum(Currency& one, Currency& two) {
         int s1, s2, s3;
         Currency answer;
-        s1= (one.rubles * 100) + one.cop;
-        s2= (two.rubles * 100) + two.cop;
+        s1 = (one.rubles * 100) + one.cop;
+        s2 = (two.rubles * 100) + two.cop;
         s3 = s1 + s2;
         one.rubles = s3 / 100;
         one.cop = s3 % 100;
@@ -151,7 +152,77 @@ public:
         answer.setCop(one.cop);
         return answer;
     }
-    //Currency operator+(Currency& one, Currency& two);
+    Currency operator+ (const Currency& secondCurrency)
+    {
+        int s1, s2, s3;
+        Currency answer;
+        s1 = (rubles * 100) + cop;
+        s2 = (secondCurrency.rubles * 100) + secondCurrency.cop;
+        s3 = s1 + s2;
+        rubles = s3 / 100;
+        cop = s3 % 100;
+        answer.setRubles(rubles);
+        answer.setCop(cop);
+        return answer;
+    }
+    Currency operator+ (const int& addRubles)
+    {
+        return Currency(rubles + addRubles, cop);
+    }
+    Currency operator- (const Currency& secondCurrency)
+    {
+        int s1, s2, s3;
+        Currency answer;
+        s1 = (rubles * 100) + cop;
+        s2 = (secondCurrency.rubles * 100) + secondCurrency.cop;
+        s3 = s1 - s2;
+        rubles = s3 / 100;
+        cop = s3 % 100;
+        answer.setRubles(rubles);
+        answer.setCop(cop);
+        return answer;
+    }
+    Currency operator* (const Currency& secondCurrency)
+    {
+        int s1, s2, s3;
+        Currency answer;
+        s1 = (rubles * 100) + cop;
+        s2 = (secondCurrency.rubles * 100) + secondCurrency.cop;
+        s3 = s1 * s2;
+        rubles = s3 / 100;
+        cop = s3 % 100;
+        answer.setRubles(rubles);
+        answer.setCop(cop);
+        return answer;
+    }
+    Currency operator/ (const Currency& secondCurrency)
+    {
+        int s1, s2, s3;
+        Currency answer;
+        s1 = (rubles * 100) + cop;
+        s2 = (secondCurrency.rubles * 100) + secondCurrency.cop;
+        s3 = s1 / s2;
+        rubles = s3 / 100;
+        cop = s3 % 100;
+        answer.setRubles(rubles);
+        answer.setCop(cop);
+        return answer;
+    }
+    Currency& operator++ () {
+        rubles++;
+        return *this;
+    }
+    Currency& operator++ (int)
+    {
+        Currency copy{ *this };
+        ++(*this);
+        return copy;
+    }
+    friend ostream& operator<<(ostream& stream, Currency& curr)
+    {
+        stream << "Рубли: " << curr.rubles << " копейки: " << curr.cop << endl;
+        return stream;
+    }
 };
 
 class Calendar {
@@ -180,19 +251,122 @@ public:
         this->year;
     }
     void printCalendar() {
-        cout << "год" << year<<"месяц"<<month<<"день"<<day;
+        cout << "год" << year << "месяц" << month << "день" << day;
     }
+};
+
+class Distance {
+private:
+    int km;
+    int m;
+public:
+    Distance(int km, int m) {
+        this->km = km;
+        this->m = m;
+    }
+    Distance() {
+        this->km = 0;
+        this->m = 0;
+    }
+    int getKm() {
+        return km;
+    }
+    int getM() {
+        return m;
+    }
+    void setkm(int km) {
+        this->km = km;
+    }
+    void setM(int m) {
+        this->m = m;
+    }
+    Distance operator+(Distance& second) {
+        int s1, s2, s3;
+        Distance answer;
+        s1 = (km * 100) + m;
+        s2 = (second.km * 100) + second.m;
+        s3 = s1 + s2;
+        km = s3 / 100;
+        m = s3 % 100;
+        answer.setkm(km);
+        answer.setM(m);
+        return answer;
+    }
+    Distance operator+(int& n) {
+        return Distance(km + n, m);
+    }
+    Distance operator-(Distance& second) {
+        int s1, s2, s3;
+        Distance answer;
+        s1 = (km * 100) + m;
+        s2 = (second.km * 100) + second.m;
+        s3 = s1 - s2;
+        km = s3 / 100;
+        m = s3 % 100;
+        answer.setkm(km);
+        answer.setM(m);
+        return answer;
+    }
+    Distance operator*(Distance& second) {
+        int s1, s2, s3;
+        Distance answer;
+        s1 = (km * 100) + m;
+        s2 = (second.km * 100) + second.m;
+        s3 = s1 * s2;
+        km = s3 / 100;
+        m = s3 % 100;
+        answer.setkm(km);
+        answer.setM(m);
+        return answer;
+    }
+    Distance operator/(Distance& second) {
+        int s1, s2, s3;
+        Distance answer;
+        s1 = (km * 100) + m;
+        s2 = (second.km * 100) + second.m;
+        s3 = s1 / s2;
+        km = s3 / 100;
+        m = s3 % 100;
+        answer.setkm(km);
+        answer.setM(m);
+        return answer;
+    }
+    Distance& operator++ () {
+        km++;
+        return *this;
+    }
+    Distance& operator++ (int)
+    {
+        Distance copy{ *this };
+        ++(*this);
+        return copy;
+    }
+    friend ostream& operator<<(ostream& stream, Distance& curr)
+    {
+        stream << "Киллометры: " << curr.km << " метры: " << curr.m << endl;
+        return stream;
+    }
+    friend istream& operator>>(istream& stream, Distance& curr) {
+        stream >> curr.km;stream>> curr.m;
+        return stream;
+    }
+    Distance& operator==(Distance& second) {
+
+    }
+    
 };
 int main()
 {
     setlocale(LC_ALL, "rus");
-    
+
     Currency n = Currency(10, 20);
     Currency g = Currency(10, 20);
-    Currency l = l.sum(n, g);
-    
-    l.printCyrrency();
-    
-    
-}
+    Currency o = Currency(4, 30);
+    Currency l = n + g;
+    Currency e = n - o;
+    cout << e;
 
+    //l.printCyrrency();
+
+
+}
